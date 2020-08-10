81759ca7dec9a0b3142234c63055ca3c20550797
Pierre Henri <payhache@gmail.com>
2020-08-10 22:09:03 +0200

    style

--- MVC/view/home_view.php
+++ MVC/view/home_view.php
@@ -7,9 +7,9 @@
 <body>
     <a class="btn btn-secondary m-2" href="index.php?controller=article&action=writeArticle"> Ajouter un Article</a>
     <h1 class="m-5 text-center"> Mon super blog !</h1>
-    <div class="containair  m-auto w-75">
+    <div class="containair  m-auto  w-75">
     <?php foreach ($articles as $article) { ?>
-            <h2><?= $article->getTitle(); ?></h2>
+            <h2 class="mt-5"><?= $article->getTitle(); ?></h2>
             <p><?= $article->getcontent(); ?></p>
             <p><?= $article->getAuthor(); ?></p>
             <a class="btn btn-primary m-2" href="index.php?controller=detail&id=<?= $article->getId() ?>">Detail article</a>
