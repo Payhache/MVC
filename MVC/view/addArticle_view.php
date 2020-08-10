b95357b02e9d0ef5387c125bcb33b348d818a5c1
Pierre Henri <payhache@gmail.com>
2020-08-10 21:52:30 +0200

    mise en forme de l'ajout d'article

--- MVC/view/addArticle_view.php
+++ MVC/view/addArticle_view.php
@@ -1,12 +1,32 @@
-<h1> Racontez nous tout :D</h1>
+<html>
+    <head>
+        <?php require_once('parts\meta.html'); ?>
+    </head>
+    <body>
+        <h1> Racontez nous tout :D</h1>
 
 
-<form action="index.php?controller=article&action=postArticle" method="post">
-<label for="title">titre de votre article</label>
-<input type="text" name="title">
-<label for="content">contenu de votre article</label>
-<input type="textarea" name="content">
-<label for="author">Auteur</label>
-<textarea  name="author"> </textarea>
-<button type="submit"> Envoyer Article</button>
-</form>
+        <form class="w-75 m-auto" action="index.php?controller=article&action=postArticle" method="post">
+            <div class="form-group">
+                <label for="title">titre de votre article</label>
+                <input class="form-control w-75" type="text" name="title">
+            </div>
+
+            <div class="form-group">
+                <label for="content">contenu de votre article</label>
+                <textarea class="form-control w-75" rows="15"  name="content"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="author">Auteur</label>
+                <input class="form-control w-75" type="textarea" name="author">
+            </div>
+            <button class="btn btn-primary" type="submit"> Envoyer Article</button>
+        </form>
+    </body>
+</html>
+
+
+
+
+
