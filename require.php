f935c6d7729fbf9ad675bd5054728320a3afcab6
payhache <payhache@gmail.com>
2020-08-11 16:00:51 +0200

    création du commentmanager

--- require.php
+++ require.php
@@ -1,6 +1,8 @@
 <?php
 require_once('MVC\model\manager\DbManager.php');
 require_once('MVC\model\manager\ArticleManager.php');
+require_once('MVC\model\manager\CommentManager.php');
 require_once('MVC\model\class\Article.php');
+require_once('MVC\model\class\Comment.php');
 require_once('MVC\controler\DefaultController.php');
 require_once('MVC\controler\ArticleController.php');
