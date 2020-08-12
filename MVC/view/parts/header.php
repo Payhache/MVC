50680c7a292e642c98be0d24c9dda81bb63a52cc
Pierre Henri <payhache@gmail.com>
2020-08-12 21:56:31 +0200

    formulaire login

--- MVC/view/parts/header.php
+++ MVC/view/parts/header.php
@@ -1,7 +1,12 @@
-
 <header>
     <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand mb-0 h1 btn btn-secondary m-2" href="index.php"> Accueil </a>
+        <a class="navbar-brand mb-0 h1 btn btn-secondary m-2" href="index.php"> Accueil </a>
         <a class="btn btn-secondary m-2" href="index.php?controller=article&action=writeArticle"> Ajouter un Article</a>
+        <form action="/action_page.php">
+            <input type="email" class="" id="email" placeholder="email">
+            <input type="password" class="" id="password" aria-describedby="password" placeholder="password">
+            <small id="password" class="form-text text-muted"> S'inscrire </small>
+            <button class="btn btn-secondary btn-block" type="submit">Login</button>
+        </form>
     </nav>
 </header>
